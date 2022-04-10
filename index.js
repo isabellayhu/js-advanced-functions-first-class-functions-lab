bd9e018c51e4736412fe9114728d41ec1634b881
Isabella <isabella.y.hu@gmail.com>
2022-04-10 21:07:31 +1000

    commit tree 60f69a3d8d1080099781ae3f00c597934ffdf4d0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare) {return multiplier*fare};
+}
+
+function fareDoubler(fare){
+    return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers,selector){
+    return selector(arrayOfDrivers);
+}
